b642b483124727282080e21de52ce851e51e4bd6
Greg Conant <greg.conant@gmail.com>
2012-09-05 20:24:16 -0400

    gc - cleaning up files and making it easier to run jslint (also it now lints the jakefile)

---
+++ jakefile.js
@@ -0,0 +1,42 @@
+(function () {
+    "use strict";
+
+    desc("Build and test");
+    task("default", ["lint"]);
+
+    desc("Lint everything");
+    task("lint", [], function() {
+        var lint = require("./build/lint/lint_runner.js");
+
+        var files = new jake.FileList();
+        files.include("**/*.js");
+        files.exclude("node_modules");
+        var options = nodeLintOptions();
+        lint.validateFileList(files.toArray(), options, {});
+    });
+
+    desc("Integrate");
+    task("integrate", ["default"], function() {
+       console.log("1. these are the steps to integrate");
+    });
+
+    function nodeLintOptions() {
+        return {
+            bitwise: true,
+            curly: false,
+            eqeqeq: true,
+            forin: true,
+            immed: true,
+            latedef: true,
+            newcap: true,
+            noarg: true,
+            noempty: true,
+            nonew: true,
+            regexp: true,
+            undef: true,
+            strict: true,
+            trailing: true,
+            node: true
+        };
+    };
+}());

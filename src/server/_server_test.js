72837b12ceadf12b0fc006ab0f369c3dd8f24e7b
Greg Conant <greg.conant@gmail.com>
2012-09-07 07:39:41 -0400

    gc - having first http server test interact with server code. Still some issues with the process; need to get these resolved. We will see what the next episodes bring.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,8 +1,12 @@
 "use strict";
 
 var server = require("./server.js");
+var http = require("http");
 
 exports.testHttpServer = function (test) {
+    server.start();
+    http.get("http://localhost:8080", function (response) {
 
+    });
     test.done();
 };
--- src/server/server.js
+++ src/server/server.js
@@ -1,5 +1,19 @@
 "use strict";
 
-exports.number = function () {
-    return 3;
+var http = require("http");
+var myServer;
+
+exports.start = function () {
+    var server = http.createServer();
+
+    myServer = server;
+    server.on("request", function (request, response) {
+        console.log("Received request");
+        var body = "<html><head><title>Node HTTP Test</title><body><p>This is a basic response for node's HTTP server.</p>" +
+            "</body></html>";
+
+        response.end(body);
+    });
+
+    server.listen(8080); // TODO: remove duplication of port #
 };

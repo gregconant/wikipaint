e108c15e41fe981a62f6f8dba705b294a005aada
Greg Conant <greg.conant@gmail.com>
2012-09-12 07:41:41 -0400

    gc - got first test working for server responding to GET request

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -4,10 +4,10 @@ var server = require("./server.js");
 var http = require("http");
 
 
-exports.tearDown = function (test) {
+exports.tearDown = function (done) {
     console.log("tear down start");
     server.stop(function () {
-        console.log("stop callback");
+        done();
     });
     console.log("tear down end");
 };
@@ -15,14 +15,9 @@ exports.tearDown = function (test) {
 // TODO: handle case where stop() is called before start()
 // TODO: test-drive stop() callback
 
-exports.testHttpServer = function (test) {
+exports.testServerRespondsToGetRequests = function (test) {
     server.start();
-
     http.get("http://localhost:8080", function (response) {
-        console.log("response callback");
         test.done();
     });
-    console.log("get called");
-
-    console.log("done called");
 };
--- src/server/server.js
+++ src/server/server.js
@@ -6,15 +6,11 @@ var server;
 
 exports.start = function () {
     server = http.createServer();
-
     server.on("request", function (request, response) {
-        console.log("Received request");
         var body = "<html><head><title>Node HTTP Test</title><body><p>This is a basic response for node's HTTP server.</p>" +
             "</body></html>";
-
         response.end(body);
     });
-
     server.listen(8080); // TODO: remove duplication of port #
 };
 

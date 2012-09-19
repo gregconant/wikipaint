5172b0add996b3e81e6e8432ba4c3f0f2fdad78b
Greg Conant <greg.conant@gmail.com>
2012-09-18 21:59:24 -0400

    gc - server is serving a static file as provided by the tests

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -2,42 +2,39 @@
 
 var PORT = "8081";
 
-var server = require("./server.js");
-var http = require("http");
-var fs = require("fs");
+var server = require("./server.js"),
+    http = require("http"),
+    fs = require("fs"),
+    assert = require("assert"),
+    TEST_FILE = "generated/test/test.html";
 
-//exports.setUp = function (done) {
-//    server.start(8080);
-//    done();
-//};
+exports.tearDown = function (done) {
+    if (fs.existsSync(TEST_FILE)) {
+        fs.unlinkSync(TEST_FILE);
+        assert.ok(!fs.existsSync(TEST_FILE), "could not delete test file: [" + TEST_FILE + "]");
+    }
+    done();
+};
 
-//exports.tearDown = function (done) {
-//    console.log("tear down start");
-//    server.stop(function () {
-//        done();
-//    });
-//    console.log("tear down end");
-//};
+// TODO: move file cleanup to "teardown" method
+exports.test_serverServesAFile = function (test) {
+    var testDir = "generated/test",
+        testData = "This is served from a file",
+        request;
 
-// test redundant given the ReturnsHelloWorld test below
-//exports.test_serverRespondsToGetRequests = function (test) {
-//    server.start();
-//    http.get("http://localhost:8080", function (response) {
-//        test.done();
-//    });
-//};
+    fs.writeFileSync(TEST_FILE, testData);
 
-exports.test_serverReturnsHelloWorldOnGivenPort = function (test) {
-    server.start(PORT);
-    var request = http.get("http://localhost:" + PORT);
+    server.start(TEST_FILE, PORT);
+    request = http.get("http://localhost:" + PORT);
     request.on("response", function (response) {
         var receivedData = false;
         response.setEncoding("utf8");
+
         test.equals(200, response.statusCode, "status code");
         response.on("data", function (chunk) {
-            test.equals("Hello World", chunk, "response text");
-            console.log("got data!");
             receivedData = true;
+            test.equals(testData, chunk, "response text");
+
         });
         response.on("end", function () {
             test.ok(receivedData, "should have received response data");
@@ -49,15 +46,22 @@ exports.test_serverReturnsHelloWorldOnGivenPort = function (test) {
     });
 };
 
-exports.test_serverRequiresPortNumber = function (test) {
+exports.test_serverRequiresFileToServe = function (test) {
     test.throws(function () {
         server.start();
     });
     test.done();
 };
 
+exports.test_serverRequiresPortNumber = function (test) {
+    test.throws(function () {
+        server.start(TEST_FILE);
+    });
+    test.done();
+};
+
 exports.test_serverRunsCallbackWhenStopCompletes = function (test) {
-    server.start(PORT);
+    server.start(TEST_FILE, PORT);
     server.stop(function () {
         test.done();    // this proves that the callback is being called, because the test is ending.
     });
@@ -70,15 +74,3 @@ exports.test_stopCalledWhenServerIsntRunning_ThrowsException = function (test) {
     test.done();
 };
 
-exports.test_serverServesAFile = function (test) {
-    var testDir = "generated/test",
-        testFile = testDir + "/test.html";
-    try {
-        fs.writeFileSync(testFile, "Hello World");
-        test.done();
-    } finally {
-        fs.unlinkSync(testFile);
-        test.ok(!fs.existsSync(testFile), "file should have been deleted.");
-    }
-
-};
--- src/server/server.js
+++ src/server/server.js
@@ -1,17 +1,22 @@
 "use strict";
 
-var http = require("http");
-var server;
+var http = require("http"),
+    fs = require("fs"),
+    server;
 
 
-exports.start = function (portNumber) {
+exports.start = function (htmlFileToServe, portNumber) {
     if (!portNumber) {
         throw "port number is required";
     }
     server = http.createServer();
     server.on("request", function (request, response) {
-        var responseText = "Hello World";
-        response.end(responseText);
+        fs.readFile(htmlFileToServe, function (err, data) {
+            if (err) {  // TODO: fix me
+                throw err;
+            }
+            response.end(data);
+        });
     });
     server.listen(portNumber);
 };

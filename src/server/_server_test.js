ec49b5f20b996a18abcffcdfa3ae74dbc4125665
Greg Conant <greg.conant@gmail.com>
2012-09-20 07:31:38 -0400

    gc - changing port number in HTTP server tests to use port 8081 always, as 8080 is sometimes being used on this machine.

--- src/server/_server_test.js
+++ src/server/_server_test.js
@@ -1,7 +1,7 @@
 (function () {
     "use strict";
 
-    var PORT = "8080",
+    var PORT = "8081",
         server = require("./server.js"),
         http = require("http"),
         fs = require("fs"),
@@ -10,17 +10,17 @@
 
 
     function httpGet(url, callback) {
-        server.start(TEST_FILE, 8080);
+        server.start(TEST_FILE, PORT);
         var request = http.get(url);
-        request.on("response", function(response) {
+        request.on("response", function (response) {
             var receivedData = "";
             response.setEncoding("utf8");
 
-            response.on("data", function(chunk) {
+            response.on("data", function (chunk) {
                 receivedData += chunk;
             });
-            response.on("end", function() {
-                server.stop(function() {
+            response.on("end", function () {
+                server.stop(function () {
                     callback(response, receivedData);
                 });
             });
@@ -34,12 +34,12 @@
         }
         done();
     };
-    exports.test_serverServesHomePageFromFile = function(test) {
-        var testDir = "generated/test";
-        var testData = "This is served from a file";
+    exports.test_serverServesHomePageFromFile = function (test) {
+        var testDir = "generated/test",
+            testData = "This is served from a file";
 
         fs.writeFileSync(TEST_FILE, testData);
-        httpGet("http://localhost:8080", function(response, responseData) {
+        httpGet("http://localhost:" + PORT, function (response, responseData) {
             test.equals(200, response.statusCode, "status code");
             test.equals(testData, responseData, "response text");
             test.done();
@@ -47,18 +47,18 @@
     };
 
     exports.test_serverReturns404ForEverythingExceptHomePage = function (test) {
-        httpGet("http://localhost:8080/bargle", function(response, responseData) {
+        httpGet("http://localhost:" + PORT + "/bargle", function (response, responseData) {
             test.equals(404, response.statusCode, "status code");
             test.done();
         });
     };
 
     exports.test_serverReturnsHomePageWhenAskedForIndex = function (test) {
-        var testDir = "generated/test";
-        var testData = "This is served from a file";
+        var testDir = "generated/test",
+            testData = "This is served from a file";
 
         fs.writeFileSync(TEST_FILE, testData);
-        httpGet("http://localhost:8080/index.html", function(response, responseData) {
+        httpGet("http://localhost:" + PORT + "/index.html", function(response, responseData) {
             test.equals(200, response.statusCode, "status code");
             test.done();
         });

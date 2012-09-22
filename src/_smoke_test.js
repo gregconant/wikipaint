61172ef121070d87ae312c8f72358b9fedcdcf62
Greg Conant <greg.conant@gmail.com>
2012-09-22 14:23:35 -0400

    gc - filling out smoke_test.js so it can try to start the server with node and make sure it's running. tests still failing.

--- src/_smoke_test.js
+++ src/_smoke_test.js
@@ -13,23 +13,56 @@
 (function () {
     "use strict";
 
-    var child_process = require("child_process");
+    var child_process = require("child_process"),
+        http = require("http"),
+        server = require("./server/server.js"),
+        jake = require("jake");
 
     exports.test_for_smoke = function (test) {
-        var command = "node wikipaint 8080";
-        child_process.exec(command, function (error, stdout, stderr) {
-            if (error !== null) {
-                console.log(stderr);
-                console.log(stdout);
-                throw error;
-            }
-            console.log("callback");
-            test.done();
-        });
+
+        var nodeArgs = ["src/server/wikipaint 8081"];
+
+        runServer(nodeArgs);
+        setTimeout(function () {
+            console.log("ran server");
+            httpGet("http://localhost:8080", function (response, receivedData) {
+                console.log("got file");
+                test.done();
+            });
+        }, 1000);
     };
 
-    function runProcess(command) {
+    function runServer(nodeArgs) {
+        var process = child_process.spawn("node", nodeArgs);
 
+        process.stdout.on("data", function (chunk) {
+            console.log("server stdout: " + chunk);
+        });
+        process.stderr.on("data", function (chunk) {
+            console.log("server stderr: " + chunk);
+        });
+        process.on("exit", function (code, signal) {
+            console.log("server process exited with code [" + code + "] and signal [" + signal + "]");
+        });
+//        process.run();
     }
 
+    // TODO: eliminate duplication with same function in _server_test.js
+    function httpGet(url, callback) {
+
+        var request = http.get(url);
+        request.on("response", function (response) {
+            var receivedData = "";
+            response.setEncoding("utf8");
+
+            response.on("data", function (chunk) {
+                receivedData += chunk;
+            });
+            response.on("end", function () {
+                server.stop(function () {
+                    callback(response, receivedData);
+                });
+            });
+        });
+    }
 }());
---
+++ src/server/wikipaint.js
@@ -0,0 +1,13 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: grewgsmash
+ * Date: 9/22/12
+ * Time: 1:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function () {
+    "use strict";
+
+    var server = require("./server.js");
+    //server.start("homepage.html", "404.html", 8081);
+}());

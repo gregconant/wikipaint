5bb3f4831253ddab3b5ca8c109993b3e3d9619ae
Greg Conant <greg.conant@gmail.com>
2012-09-22 11:18:51 -0400

    gc - adding placeholder for smoke test for launching server and confirming it works

---
+++ src/_smoke_test.js
@@ -0,0 +1,16 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: grewgsmash
+ * Date: 9/22/12
+ * Time: 11:16 AM
+ * To change this template use File | Settings | File Templates.
+ */
+
+// launch the server in the same way it happens in production
+// get a page
+// confirm we got something
+
+(function () {
+    "use strict";
+
+}());

2f8ceb0f25c9a80735ce6dd869d6bd1670b26ff1
Greg Conant <greg.conant@gmail.com>
2012-11-29 22:03:43 -0500

    gc - done this episode -- mostly just documenting what we want to do and modifying client tests slightly to start trying to assert against the DOM. have failing tests now, assume we will fix this soon.

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,13 +1,13 @@
-/*global describe, it, expect*/
+/*global describe, it, expect, dump*/
 
 (function () {
     "use strict";
-//var assert = cha.assert;
 
     describe("Nothing", function () {
 
         it("should run", function () {
-            expect("test").to.equal("test");
+            dump(document.body);
+
         });
 
     });

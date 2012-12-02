f1d6fc1af1f31eafecef6d360b977b40a825e37e
Greg Conant <greg.conant@gmail.com>
2012-12-02 09:36:11 -0500

    gc - inserted element into Testacular DOM through a test.

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -8,6 +8,7 @@
         it("should run", function () {
             var div = document.createElement("div");
             div.setAttribute("id", "tdjs");
+            div.setAttribute("foo", "bar");
             document.body.appendChild(div);
             dump("breakpoint here");
         });

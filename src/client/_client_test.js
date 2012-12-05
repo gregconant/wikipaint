ea0da02b0e1f0ed981be07a6157b70dd0df46f34
Greg Conant <greg.conant@gmail.com>
2012-12-04 20:15:37 -0500

    gc - wrote an afterEach function to clean up the DOM after each test.

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -1,9 +1,13 @@
-/*global describe, it, expect, dump, require, $, wikiPaint*/
+/*global describe, it, expect, afterEach, dump, require, $, wikiPaint*/
 
 (function () {
     "use strict";
 
     describe("Drawing area", function () {
+
+        afterEach(function () {
+           $("#wikipaint-drawing-area").remove();
+        });
 
         it("should be initialized in predefined div", function () {
             // create div that's assumed to be in our home page
@@ -28,6 +32,18 @@
         });
 
         it("should have the same dimensions as its enclosing div", function () {
+            // assert that DOM element from above test does not endure
+            var div = $("#wikipaint-drawing-area");
+            expect(div.length).to.equal(0);
+
+            // create div that's assumed to be in our home page
+            var testHtml = "<div id='my-test-div' style='height: 200px; width:400px; border:1px solid yellow;'>Hi, jerk.</div>",
+                drawingAreaId = "wikipaint-drawing-area";
+
+            $("body").append(testHtml);
+            dump("hi");
+            var element = $("body").find("#my-test-div");
+            expect(element).to.be.ok();
 
         });
     });

a85a038912f5164f5c4abeb12cf116d3a9dd30d3
Greg Conant <greg.conant@gmail.com>
2012-12-04 20:22:32 -0500

    gc - confirming in test that the created div has the right height and width

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -7,6 +7,7 @@
 
         afterEach(function () {
            $("#wikipaint-drawing-area").remove();
+            $("#my-test-div").remove();
         });
 
         it("should be initialized in predefined div", function () {
@@ -33,17 +34,21 @@
 
         it("should have the same dimensions as its enclosing div", function () {
             // assert that DOM element from above test does not endure
-            var div = $("#wikipaint-drawing-area");
+            var drawingAreaId = "#wikipaint-drawing-area",
+                testDivId = "#my-test-div",
+                div = $(drawingAreaId);
+
             expect(div.length).to.equal(0);
 
             // create div that's assumed to be in our home page
-            var testHtml = "<div id='my-test-div' style='height: 200px; width:400px; border:1px solid yellow;'>Hi, jerk.</div>",
-                drawingAreaId = "wikipaint-drawing-area";
+            var testHtml = "<div id='my-test-div' style='height: 200px; width:400px;'>Hi, jerk.</div>";
 
             $("body").append(testHtml);
             dump("hi");
-            var element = $("body").find("#my-test-div");
+            var element = $("body").find(testDivId);
             expect(element).to.be.ok();
+            expect($(testDivId).height()).to.be(200);
+            expect($(testDivId).width()).to.be(400);
 
         });
     });

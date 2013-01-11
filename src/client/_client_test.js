e66a382c5d1e61b3dbf37c4781102a999c1d6f8d
Greg Conant <greg.conant@gmail.com>
2013-01-10 19:24:20 -0500

    gc - cleaned up test helper functions.

--- src/client/_client_test.js
+++ src/client/_client_test.js
@@ -13,15 +13,15 @@
         VML_MAGIC_NUMBER = 21600;
 
     describe("Drawing area", function () {
-
         pathObjectFromRegex = function(regex){
-            return {
-                x: regex[1],
-                y: regex[2],
-                x2: regex[3],
-                y2: regex[4]
-            };
+            return [
+                regex[1],
+                regex[2],
+                regex[3],
+                regex[4]
+            ];
         };
+
 
         vmlPathFor = function(element) {
             var startX,
@@ -37,13 +37,13 @@
             endX = ie8[3] / VML_MAGIC_NUMBER;
             endY = ie8[4] / VML_MAGIC_NUMBER;
 
-            return {
-                x: startX,
-                y: startY,
-                x2: endX,
-                y2: endY
-            };
-            //return "M" + startX + "," + startY + "L" + endX + "," + endY;
+            return [
+                startX,
+                startY,
+                endX,
+                endY
+            ];
+
         };
 
         svgPathFor = function(element) {
@@ -82,31 +82,13 @@
             }
         };
 
-
         function paperPaths(paper) {
-            // note: paths are normalized with left side first in all cases
             var elements = getElements(paper),
-                result = [],
-                box;
-
+                result = [];
             for(var i = 0; i < elements.length; i +=1) {
-                box = pathFor(elements[i]);
-
-                result.push([ box.x, box.y, box.x2, box.y2]);
+                result.push(pathFor(elements[i]));
             }
             return result;
-
-        }
-
-        function clickMouse(relativeX, relativeY) {
-            var topLeftOfDrawingArea = $drawingArea.offset(),
-                pageX = relativeX + topLeftOfDrawingArea.left,
-                pageY = relativeY + topLeftOfDrawingArea.top,
-                eventData = new jQuery.Event("click");
-
-            eventData.pageX = pageX;
-            eventData.pageY = pageY;
-            $drawingArea.trigger(eventData);
         }
 
         function mouseDown(relativeX, relativeY) {
@@ -167,40 +149,6 @@
             });
             return elements;
         }
-
-        it("draws line segments in response to clicks", function () {
-            // click inside drawing area
-            var topLeftOfDrawingArea,
-                expectedX,
-                expectedY,
-                eventData;
-
-            $drawingArea = $("<div style='height: 300px; width:600px;'>Hi, jerk.</div>");
-            $("body").append($drawingArea);
-            raphPaper = wikiPaint.initializeDrawingArea($drawingArea[0]);
-
-            // these points are offsets, not absolute page points. need to add relative position.
-            clickMouse(20, 30);
-            clickMouse(50, 60);
-            clickMouse(40, 20);
-
-            //expect(paperPaths(raphPaper)).to.eql([[20, 30, 50, 60], [50, 60, 40, 20]]);
-        });
-
-        it("draws line segments in response to drags", function () {
-            $drawingArea = $("<div style='height: 300px; width:600px;'>Hi, jerk.</div>");
-            $("body").append($drawingArea);
-            raphPaper = wikiPaint.initializeDrawingArea($drawingArea[0]);
-
-            //clickAndDragMouse(20, 30, 50, 60);
-//            mouseDown(20, 30);
-            mouseMove(20, 30);
-            mouseMove(50, 60);
-//            mouseUp(50, 60);
-
-            //expect(paperPaths(raphPaper)).to.eql([[20, 30, 50, 60], [50, 60, 40, 20]]);
-        });
-
         it("draws a line in response to mouse drag", function() {
             $drawingArea = $("<div style='height: 300px; width:600px;'>Hi, jerk.</div>");
             $("body").append($drawingArea);
@@ -210,6 +158,17 @@
             mouseMove(50, 60);
 
             expect(paperPaths(raphPaper)).to.eql([ [20, 30, 50, 60] ]);
+        });
+
+        it("does not draw line segment in response to mouseup event", function() {
+            $drawingArea = $("<div style='height: 300px; width:600px;'>Hi, jerk.</div>");
+            $("body").append($drawingArea);
+            raphPaper = wikiPaint.initializeDrawingArea($drawingArea[0]);
+
+            mouseDown(20, 30);
+            mouseUp(50, 60);
+
+            expect(paperPaths(raphPaper)).to.eql([ ]);
         });
 
         it("does not draw line segments when mouse is not down", function () {
@@ -266,16 +225,6 @@
             expect(paperPaths(raphPaper)).to.eql([ [20, 30, 50, 60], [30, 25, 10, 15] ]);
         });
 
-        it("does not draw line segment in response to mouseup event", function() {
-            $drawingArea = $("<div style='height: 300px; width:600px;'>Hi, jerk.</div>");
-            $("body").append($drawingArea);
-            raphPaper = wikiPaint.initializeDrawingArea($drawingArea[0]);
-
-            mouseDown(20, 30);
-            mouseUp(50, 60);
-
-           expect(paperPaths(raphPaper)).to.eql([ ]);
-        });
 
 //        it("considers border when calculating mouse target", function () {
 //            $drawingArea = $("<div style='height: 300px; width:600px; border-width:13px;'>Hi, jerk.</div>");
